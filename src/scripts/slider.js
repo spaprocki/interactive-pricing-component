277c1bdaa0cf135f49a4b564667b3b45ea178b3b
Szymon Paprocki <s.paprocki.vs@gmail.com>
2023-02-20 15:45:20 +0100

    written html and css of accessible toggle switch

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -11,22 +11,26 @@ const sliderElement = document.getElementById('pricing-slider');
 let sliderPosition = sliderElement.value;
 const pageviewsElement = document.getElementById('pageviews');
 const priceElement = document.getElementById('price');
-const toggleElement = document.getElementById('toggle');
+const toggleElement = document.getElementById('yearly');
 
 sliderElement.addEventListener('input', (e) => {
   let sliderPosition = e.target.value;
   pageviewsElement.textContent = myMappings[sliderPosition].pageviews;
   priceElement.textContent =
     myMappings[sliderPosition].price * (1 - discount) + '.00$';
+
   e.target.style.background = `linear-gradient(90deg, #a5f3eb ${
     sliderPosition * 25
   }%, #eaeefb ${sliderPosition * 25}%)`;
+
+  sliderElement.ariaValueNow = sliderElement.rangeVal;
+  sliderElement.ariaValueText = `${pageviewsElement.textContent} page views for ${priceElement.textContent} per month`;
 });
 
 toggleElement.addEventListener('click', (e) => {
   sliderPosition = sliderElement.value;
   [e.target.value, discount] =
-    e.target.value == 'monthly' ? ['yearly', 0.25] : ['monthly', 0];
+    e.target.checked == 'monthly' ? ['yearly', 0.25] : ['monthly', 0];
   priceElement.textContent =
     myMappings[sliderPosition].price * (1 - discount) + '.00$';
 });
